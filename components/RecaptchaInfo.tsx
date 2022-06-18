df7d86929a17f5ed51df9e0c61136661820a4cfd
33 <moe@3-3.dev>
2022-06-18 20:33:19 +0800

    Tweak: reCAPTCHA tip

--- components/RecaptchaInfo.tsx
+++ components/RecaptchaInfo.tsx
@@ -1,11 +1,12 @@
 import {Grid, Link} from "@mui/material";
 import Box from "@mui/material/Box";
+import {Security} from "@mui/icons-material";
 
 export default function RecaptchaInfo() {
     return(
-        <Grid item xs={10}>
+        <Grid item xs={12}>
             <Box fontSize="0.5rem">
-                <div>本网站使用了 reCAPTCHA <Link href="https://policies.google.cn/">隐私权和条款</Link></div>
+                <Security sx={{ fontSize: 10 }}/> reCAPTCHA <Link href="https://policies.google.cn/">隐私权和条款</Link>
             </Box>
         </Grid>
     );
